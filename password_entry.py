d6636cbd8c0aa2de6da0544311dfb583afbe042e
riskacintaanettarampengan <anettarampengan@gmail.com>
2019-08-12 11:37:10 +0800

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,15 @@
 """
 Riskacinta Anetta Rampengan
 """
+
+MINIMUM_LENGTH = 4
+
+
+def main():
+    password = input("Enter password with minimum of {} characters: ".format(MINIMUM_LENGTH))
+    while len(password) < MINIMUM_LENGTH:
+        password = input("Enter password with minimum of {} characters: ".format(MINIMUM_LENGTH))
+    print('*' * len(password))
+
+
+main()
